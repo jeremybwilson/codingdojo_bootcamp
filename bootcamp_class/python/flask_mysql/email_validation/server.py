e8965da1937e47f8d82d8d100b86194635ba734e
Jeremy Wilson <jeremybwilson@gmail.com>
2018-08-21 21:19:18 -0700

    Final changes for email validation assignment

--- bootcamp_class/python/flask_mysql/email_validation/server.py
+++ bootcamp_class/python/flask_mysql/email_validation/server.py
@@ -16,10 +16,10 @@ mysql = MySQLConnector(app, 'userdemodb')
 def index():
   # an example of running a query
   # print mysql.query_db('SELECT * FROM friends')
-  email_from_db = mysql.query_db('SELECT * FROM users')
+  email_from_db = mysql.query_db('SELECT email FROM users')
 
   print "*" * 80
-  print 'Here is the query:', email_from_db
+  print 'Here are the emails in the db:', email_from_db
 
   if not 'email' in session:
     email = 'No email entered!'
@@ -28,35 +28,44 @@ def index():
 
   return render_template('index.html', email=email_from_db, title="Home")
 
-@app.route('/validate', methods=['POST'])  #
+@app.route('/validate', methods=['POST'])
 def validate():
+
+  # try:
+  #   email... = results[0]
+  # except IndexError:
+  #   ...
 
   # variable to shorten all request.form references to just 'form'
   form = request.form
   form['email']
-  # create an empty list to store errors
+  # create an empty list to store errors/successes
   errors = []
   successes = []
   valid_email = False
 
   # recall the name attributes we added to our form inputs
   # to access the data that the user input into the fields we use request.form['email']
-
   # Write query as a string.  => pass form entered email into the string
-  # validate_query1 = "SELECT COUNT(*) from users WHERE email = :email"
-  validate_query = "SELECT * FROM users WHERE email = :email"
+  validate_query = "SELECT email FROM users WHERE email = :email"
   
   # We'll then create a dictionary of data from the POST data received.
   data = {
-    'email': form['email'],
+    'email': form['email']
   }
-  
+
   # Run query, with dictionary values injected into the query.
   results = mysql.query_db(validate_query, data)
 
-  # somewhere here a comparison needs to be made between the email values in the database
-  # and the form['email'] submitted from the previous page
-  # 
+  if len(results) == 0:
+    email_from_db = "Email does not exist"
+    errors.append('That email address does not exist!')
+  else:
+    email_from_db = results[0]['email']
+
+  # print "*" * 80
+  # print "Here is the email from the form: ", form['email'], "\n"
+  # print 'Here is the email from the database:', email_from_db, '\n'
 
   # validate email address
   # if the email address is less than 1, the field is blank => error
@@ -64,8 +73,6 @@ def validate():
     errors.append('Email address cannot be blank!')
   elif not EMAIL_REGEX.match(form['email']):
     errors.append('You must enter a valid email address!')
-  # elif form['email'] == data['email']:
-  #   errors.append('That email address does not exist!')
   else:
     successes.append('Success')
 
@@ -79,28 +86,26 @@ def validate():
     if len(successes) > 0:
       print "Here are the db results: ", results, '\n'
       valid_email = True
-      # validated_email = data['email']
-      # session['validated_email'] = validated_email
+      validated_email = email_from_db
+      session['validated_email'] = validated_email
       session['success'] = True
       return redirect('/success')
 
 @app.route('/success')
 def success():
 
-  # get emails and last_updated info from dbs
+  # get emails and updated_at info from db
   query = "SELECT id, email, updated_at FROM users"
 
   results = mysql.query_db(query)
 
-  # print "Here is the validated email from the previous route: ", validated_email
-
   return render_template('success.html', user_results=results, title="Success")
 
 
-@app.route('/<user_id>/delete')
+@app.route('/<user_id>/delete', methods=['POST'])
 def delete(user_id):
   # delete query for specific user_id passed with results
-  delete_query = "DELETE * FROM users WHERE id = :specific_user_id"
+  delete_query = "DELETE FROM users WHERE id = :specific_user_id"
 
   # define user_id with dictionary
   data = {

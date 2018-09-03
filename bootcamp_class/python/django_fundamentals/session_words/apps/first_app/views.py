cb7034348c64091b567a8eb17996ec35b318d01f
Jeremy Wilson <jeremybwilson@gmail.com>
2018-09-03 11:44:58 -0700

    Updates to session words assignment files

--- bootcamp_class/python/django_fundamentals/session_words/apps/first_app/views.py
+++ bootcamp_class/python/django_fundamentals/session_words/apps/first_app/views.py
@@ -10,8 +10,13 @@ def index(request):
     return render(request, 'first_app/index.html')
 
 def process(request):
+    request.session['date_time'] = datetime.now().strftime('%H:%M:%S%p, %B %d %Y')
+    date_time = request.session['date_time']
+    print "*" * 80
+    print "Here is the current date/time: ", date_time
+
     if request.method == "POST":
-        request.session['date_time'] = datetime.now().strftime('%#H:%M:%S%p, %B %#d %Y')
+        request.session['date_time'] = datetime.now().strftime('%H:%M:%S%p, %B %d %Y')
         date_time = request.session['date_time']
         
         print "*" * 80
@@ -33,8 +38,8 @@ def process(request):
                 'session_font': 'default',
                 'session_time': date_time
             }
-        session_word = request.POST['session_word']
-        session_color = request.POST['session_color']
+        # session_word = request.POST['session_word']
+        # session_color = request.POST['session_color']
         print "Here is the passed form information: ", request.POST
 
         request.session['words'].append(data)

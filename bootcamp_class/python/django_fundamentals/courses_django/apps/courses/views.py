1ebf1a8b778562e3a1087052a6db3cd54166299b
Jeremy Wilson <jeremybwilson@gmail.com>
2018-09-23 11:44:19 -0700

    First commit

---
+++ bootcamp_class/python/django_fundamentals/courses_django/apps/courses/views.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import Course
+
+# Create your views here.
+def index(request):
+    context = {
+        "courses" : Course.objects.all(),
+    }
+    print "*" * 80
+    print "Successfully entered the add route."
+    print context
+
+    return render(request, "courses/index.html", context)
+
+def add(request):
+    if request.method == 'POST':
+
+        # errors = Course.objects.custom_form_validator(request.POST)
+        valid, result = Course.objects.custom_form_validator(request.POST)
+        print "*" * 80
+        print "Successfully entered the add route."
+
+        if valid:
+            request.session['course_id'] = result
+            # Course.objects.create(name = request.POST["course_name"], description = request.POST["desc"])
+            return redirect('courses:index')
+        else:
+            for error in result:
+                messages.error(request, error)
+            return redirect('courses:index')
+    else:
+        return redirect('courses:new')
+
+def confirm(request, course_id):
+    context = {
+        'course' : Course.objects.get(id=course_id)
+    }
+    print "*" * 80
+    print "Successfully entered the confirm route."
+    print context
+
+    return render(request, "courses/confirm.html", context)
+
+def destroy(request, course_id):
+    Course.objects.get(id=course_id).delete()
+    print "*" * 80
+    print "Successfully entered the delete route."
+
+    return redirect("/")
